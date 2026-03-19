93b2ab1f8016664a08a1a69d13d5ae7f5d4fa9d3
akaiuo <12350741+akaiuo@user.noreply.gitee.com>
2026-03-19 14:12:51 +0800

    add: 题目通过数、提交数统计 fix: validation模块pom文件缺失、启动类缺失

---
+++ whoj-backend-validation-service/src/main/java/com/whoj/whojbackendvalidationservice/WhojBackendValidationServiceApplication.java
@@ -0,0 +1,18 @@
+package com.whoj.whojbackendvalidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+@SpringBootApplication
+@ComponentScan("com.whoj") // 用于扫描到 common 模块下的全局异常处理器
+@EnableFeignClients(basePackages = {"com.whoj.whojbackendserviceclient.service"})
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@EnableDiscoveryClient
+public class WhojBackendValidationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WhojBackendValidationServiceApplication.class, args);
+    }
+}
